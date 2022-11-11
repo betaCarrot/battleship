c03e6379fc7c59922aa93d02b88ea230eab7acd6
betacarrot <zrenah@connect.ust.hk>
2022-11-11 20:43:23 +0800

    Initial Commit

---
+++ game_server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const session = require("express-session");
+
+// Create the Express app
+const app = express();
+
+// Use the 'public' folder to serve static files
+app.use(express.static("public"));
+
+// Use the json middleware to parse JSON data
+app.use(express.json());
+
+// Use the session middleware to maintain sessions
+const chatSession = session({
+    secret: "game",
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+        maxAge: 300000
+    }
+});
+app.use(chatSession);
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.use((socket, next) => {
+    chatSession(socket.request,
+        {}, next);
+});
+
+httpServer.listen(8000, () => {
+    console.log("The game server has started...");
+});
